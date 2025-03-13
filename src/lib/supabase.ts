95128b45ca1c6a7e98dee4148d3ace41b31c5864
AbhivirajG <ag10158@stern.nyu.edu>
2025-03-13 01:22:48 -0400

    Updates

--- src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://gzqhveimckwkljjevvzg.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6cWh2ZWltY2t3a2xqamV2dnpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE4MzY2MzEsImV4cCI6MjA1NzQxMjYzMX0.1t6Ea4lR53B37GAt2BRO0IZ82IfgtkoPDua5ScwOYfM';
-
-export const supabase = createClient(supabaseUrl, supabaseKey); 
